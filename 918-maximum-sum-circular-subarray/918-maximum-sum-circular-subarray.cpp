732bd774bd85b67b28d25626c0e29d3fc5926ea1
spythrone <pragyat021@gmail.com>
2022-09-24 19:25:34 +0530

    Time: 140 ms (26.37%), Space: 39.9 MB (71.68%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
@@ -1,15 +1,23 @@
-class Solution:
+class Solution {
+public:
     
-    def max_sub_arr(self, nums:List[int]) -> int:
-        mx_flg = -30001
-        mx_sm = -30001
-        for i in nums:
-            mx_flg = max(mx_flg+i, i)
-            mx_sm = max(mx_flg, mx_sm)
-        return mx_sm
+    int maxSubarray(vector<int>& nums){
+        int mx_flg = 0;
+        int mx_sum = INT_MIN;
+        for(int i=0; i<nums.size(); i++){
+            mx_flg = max(mx_flg+nums[i], nums[i]);
+            mx_sum = max(mx_sum, mx_flg);
+        }
+        return mx_sum;
+    }
     
-    def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        sm_arr = sum(nums)
-        mx_sum = self.max_sub_arr(nums)
-        min_sum = self.max_sub_arr(list(map(lambda item: item * -1, nums)))*-1
-        return  max(mx_sum, (sm_arr - min_sum)) if mx_sum>0 else mx_sum 
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int sum_arr = accumulate(nums.begin(), nums.end(), 0);
+        int max_sum = maxSubarray(nums);
+        transform(nums.begin(), nums.end(), nums.begin(), bind1st(std::multiplies<int>(), -1));
+        int min_sum = maxSubarray(nums)*-1;
+        cout<<"mx sum"<<max_sum<<"\n";
+        cout<<"min sum"<<min_sum;
+        return max_sum>0? max(max_sum, (sum_arr - min_sum)): max_sum;
+    }
+};
