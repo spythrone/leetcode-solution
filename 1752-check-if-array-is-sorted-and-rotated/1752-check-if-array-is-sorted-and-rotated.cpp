e2137dac0a8000f7dca953879ed5aa569b70d41d
spythrone <pragyat021@gmail.com>
2022-09-27 18:59:55 +0530

    Time: 8 ms (13.08%), Space: 8.9 MB (36.91%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
-    
-    void swap_vector(vector<int>& nums, int start, int end){
-        int mid = start + (end - start -1)/2;
-        for(int i=start; i<=mid; i++){
-            swap(nums[i], nums[end]);
-            end--;
-        }
-    }
-    
     bool check(vector<int>& nums) {
-        int breakpoint = 0;
-        if(nums.size()==1) return true;
-        int flag = 0;
-        for(int i=0; i<nums.size()-1; i++){
-            if(nums[i]>nums[i+1]){
-                flag =1;
-                break;   
-            } 
+        queue<int> q1;
+        queue<int> q2;
+        for(auto x : nums){
+            q1.push(x);
         }
-        if(flag == 0) return true;
-        for(int i=0; i<nums.size()-1; i++){
-            if(nums[i]>nums[i+1]){
-                breakpoint = i;
-                break;
-            }
+        vector<int> t = nums;
+        sort(t.begin(), t.end());
+        for(auto x : t){
+            q2.push(x);
         }
-        swap_vector(nums, 0, breakpoint);
-        swap_vector(nums, breakpoint+1, nums.size()-1);
-        swap_vector(nums, 0, nums.size()-1);
-        for(int i=0; i<nums.size()-1; i++){
-            if(nums[i]>nums[i+1]) return false;
+        for(int i=0; i<nums.size(); i++){
+            if(q1 == q2) return true;
+            int fr = q1.front();
+            q1.pop();
+            q1.push(fr);
         }
-        return true;
+        return false;
     }
 };
